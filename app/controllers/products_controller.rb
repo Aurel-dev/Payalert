766143d69f163919729073ad26cf74f8ea380606
Maxence Heyndrickx <m.heyndrickx@hotmail.fr>
2020-06-23 16:38:53 +0200

    search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,35 @@
 class ProductsController < ApplicationController
-def new
+def index 
+    @products = Product.all
 end
 
+def new
+    @product = Product.new
+end
+
+def create
+    @product = Product.new(product_params)
+    if @product.save
+        redirect to @product
+    else
+        render :new
+    end
+end
+
+def edit
+end
+
+def destroy
+@product.destroy
+end
+
+private
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.requis(:product).permit(:name, :description)
+  end
 end
