d32596ea4861d43e185c662bd36143edf5bee3a7
alexandre Bordeyne <bordeynea@gmail.com>
2020-06-24 16:48:12 +0200

    show

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,15 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/profile", to: "profile#show"
+resources :products do
+resources :paylerts ,only: [:create , :new]
 
-
+end
 
  # url "payalert/search"
   get "/search", to: "pages#search"
   # url "payalert/products" controller pages
-  get "/products", to: "products#index"
+  
 
 resources :users do 
   resources :profile
