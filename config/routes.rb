d271114392a1b2f0adbb728e182d67d1e469cf93
aurelien dhellemme <dhellemme.a@gmail.com>
2020-06-24 17:52:04 +0200

    123

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get "/profile", to: "profile#show"
+  
 resources :products do
 resources :paylerts ,only: [:create , :new]
 
