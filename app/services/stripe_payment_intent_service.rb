842e81d17bfe515669619718d79d96b9fccd82fd
aurelien dhellemme <dhellemme.a@gmail.com>
2020-06-30 17:30:46 +0200

    configcbornot

--- app/services/stripe_payment_intent_service.rb
+++ app/services/stripe_payment_intent_service.rb
@@ -2,7 +2,6 @@ class StripePaymentIntentService
     def call(event)
       stripe_customer_id = event.data.object.customer
       credit_card_id = event.data.object.payment_method
-   
       user = User.find_by(stripe_customer_id: stripe_customer_id)
       user.credit_card_id = credit_card_id
       user.save!
